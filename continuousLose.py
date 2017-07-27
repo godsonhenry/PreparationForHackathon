4df72dbfd8d631cb1ebe6d4afd06d392aad1ae33
godsonhenry <godsonhenry@users.noreply.github.com>
2017-07-27 12:29:56 -0400

    Update continuousLose.py

--- continuousLose.py
+++ continuousLose.py
@@ -1,16 +1,11 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Wed Jul 26 21:46:05 2017
-
-@author: wuyuhang
-"""
 import random
 
 '''
 Calculate the probability of continuous lose of a team in a regular season. The probability of winning is 0.8 for each game. 
 '''
 n=10000000                # times of simulation
-lst=[]
+count=0                   # count the possible season in which continous lose happens.
 for i in range(n):
     match=1
     lose=0
@@ -19,11 +14,9 @@ for i in range(n):
         if x<=0.2:
             lose+=1      # calcuate the losing game
         else:
-            lose==0     # if team win, the lose count reset to be 0.
+            lose=0     # if team win, the lose count reset to be 0.
         if lose==2:
-            lst.append(1) # if continous lose happens, we mark that event as 1, and finish this round of simulation
-            break
-        lst.append(0)    # if continous lose didn't happen in whole regular season, then we mark that event as 0
+            count+=1 # if continous lose happens, count increase 1, and finish this round of simulation
+            break         
         match+=1            # if continous lose didn;t happen until current game, the regular season contunues.
-print(lst.count(1)/len(lst)) # calculate the sample probability of continuous lose. result is around 10%
-        
+print(count/n )# calculate the sample probability of continuous lose. result is around 94%
